a535cea3dceebfd53709031ff45c4effe1f8ada7
akashsj22 <akashsj2201@gmail.com>
2026-01-02 21:04:18 +0530

    Update and rename c to 1.c

---
+++ 1.c
@@ -0,0 +1,6 @@
+#include <stdio.h> // Includes standard input/output library
+
+int main() { // The main function where execution begins
+  printf("Hello, C World!\n"); // Prints text to the console
+  return 0; // Indicates successful execution
+}
