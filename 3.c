89395475907e411e2acda333f573d5be6acd399c
akashsj22 <akashsj2201@gmail.com>
2026-01-04 18:41:31 +0530

    3.c

---
+++ 3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int value = 10;   
+    printf("%d\n", value);
+    return 0;
+}
+
+#include <stdio.h>
+
+int main() {
+    int num = 10;
+    if (num > 5) {
+        printf("Greater than 5");
+    }
+    return 0;
+}
