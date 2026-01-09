5ee1432d52e6b8f7a391e693d197933a3c4a2eb8
akashsj22 <akashsj2201@gmail.com>
2026-01-09 10:05:25 +0530

    8.c

---
+++ 8.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main() {
+    for (int i = 0; i < 3; i++) {
+        printf("Hello GfG\n");
+    }
+    return 0;
+}
+
+#include <stdio.h>
+
+int main() {
+    for (int i = 0; i < 5; i++) {
+        printf( "%d ", i + 1);
+    }
+    
+    return 0;
+}
+
+#include <stdio.h>
+int main() {
+  int i = 0; 
+  while(i <= 5) {
+    printf("%d ", i + 1);   
+    i++;
+  } 
+  return 0;
+}
+
+#include <stdio.h>
+ 
+int main() {
+  int i = 0; 
+  
+  do
+  {
+    printf( "%d ", i);   
+    i++;
+  }  while (i <= 10);   
+  
+  return 0;
+}
