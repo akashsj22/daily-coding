0081fbddaa6074ef745142f6b7c879f4b7ad099a
akashsj22 <akashsj2201@gmail.com>
2026-01-19 20:36:43 +0530

    18.c

---
+++ 18.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int countDigits(int n) {
+    int count = 0;
+
+    if (n == 0)
+        return 1;
+
+    while (n > 0) {
+        count++;
+        n /= 10;
+    }
+    return count;
+}
