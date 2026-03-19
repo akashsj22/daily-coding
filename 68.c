9198b720202ea020555ff075df5a2f79a9a232e2
akashsj22 <akashsj2201@gmail.com>
2026-03-19 21:30:01 +0530

    68.c

---
+++ 68.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *next;
+};
+
+struct node *head = NULL;
+
+// DELETE FIRST NODE
+void removeNode() {
+    if(head == NULL) {
+        printf("List empty\n");
+        return;
+    }
+
+    struct node *temp = head;
+    head = head->next;
+    free(temp);
+}
