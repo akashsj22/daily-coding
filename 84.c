e9b6150d208e9ca30cf34d1b9c0003f8ed58a593
akashsj22 <akashsj2201@gmail.com>
2026-04-06 21:29:51 +0530

    84.c

---
+++ 84.c
@@ -0,0 +1,29 @@
+int findMin(struct node* root)
+{
+    if (root == NULL)
+        return 9999;
+
+    int left = findMin(root->left);
+    int right = findMin(root->right);
+
+    int min = root->data;
+    if (left < min) min = left;
+    if (right < min) min = right;
+
+    return min;
+}
+
+int findMax(struct node* root)
+{
+    if (root == NULL)
+        return -9999;
+
+    int left = findMax(root->left);
+    int right = findMax(root->right);
+
+    int max = root->data;
+    if (left > max) max = left;
+    if (right > max) max = right;
+
+    return max;
+}
