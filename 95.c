782781e93b1ec1be1f9112778d46408ba55d1fa8
akashsj22 <akashsj2201@gmail.com>
2026-04-23 20:54:58 +0530

    95.c

---
+++ 95.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int linearSearch(int arr[], int n, int x)
+{
+    int i;
+    for(i = 0; i < n; i++)
+    {
+        if(arr[i] == x)
+            return i;
+    }
+    return -1;
+}
+
+int main()
+{
+    int arr[] = {5, 8, 12, 20, 30};
+    int x = 20;
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int result = linearSearch(arr, n, x);
+
+    if(result == -1)
+        printf("Element not found");
+    else
+        printf("Element found at index %d", result);
+
+    return 0;
+}
