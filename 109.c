e8ccf3a14b56c5868131538336ffb2f28f46ecea
akashsj22 <akashsj2201@gmail.com>
2026-05-10 20:23:59 +0530

    109.c

---
+++ 109.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+#define V 5
+
+int maxKey(int key[], int visited[]) {
+    int max = -1, index;
+
+    for (int i = 0; i < V; i++) {
+        if (!visited[i] && key[i] > max) {
+            max = key[i];
+            index = i;
+        }
+    }
+
+    return index;
+}
+
+void maxST(int graph[V][V]) {
+    int parent[V];
+    int key[V];
+    int visited[V];
+
+    for (int i = 0; i < V; i++) {
+        key[i] = -1;
+        visited[i] = 0;
+    }
+
+    key[0] = 0;
+    parent[0] = -1;
+
+    for (int count = 0; count < V - 1; count++) {
+        int u = maxKey(key, visited);
+        visited[u] = 1;
+
+        for (int v = 0; v < V; v++) {
+            if (graph[u][v] && !visited[v] &&
+                graph[u][v] > key[v]) {
+
+                key[v] = graph[u][v];
+                parent[v] = u;
+            }
+        }
+    }
+
+    printf("Edge Weight\n");
+
+    for (int i = 1; i < V; i++)
+        printf("%d - %d %d\n", parent[i], i, graph[i][parent[i]]);
+}
+
+int main() {
+    int graph[V][V] = {
+        {0,2,0,6,0},
+        {2,0,3,8,5},
+        {0,3,0,0,7},
+        {6,8,0,0,9},
+        {0,5,7,9,0}
+    };
+
+    maxST(graph);
+
+    return 0;
+}
