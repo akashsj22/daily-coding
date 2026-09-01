07e3de25eb7d04fc9084deb69f7e816659349baa
akashsj22 <akashsj2201@gmail.com>
2026-09-01 21:58:53 +0530

    183.c

---
+++ 183.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+#define MAX 5
+
+int stack[MAX];
+int top = -1;
+
+void push(int x) {
+    if (top == MAX - 1) {
+        printf("Stack Overflow\n");
+        return;
+    }
+    stack[++top] = x;
+}
+
+void pop() {
+    if (top == -1) {
+        printf("Stack Underflow\n");
+        return;
+    }
+    printf("Deleted Element = %d\n", stack[top--]);
+}
+
+void peek() {
+    if (top == -1)
+        printf("Stack is Empty\n");
+    else
+        printf("Top Element = %d\n", stack[top]);
+}
+
+void display() {
+    if (top == -1) {
+        printf("Stack is Empty\n");
+        return;
+    }
+
+    printf("Stack Elements: ");
+
+    for (int i = top; i >= 0; i--)
+        printf("%d ", stack[i]);
+
+    printf("\n");
+}
+
+int main() {
+    push(10);
+    push(20);
+    push(30);
+
+    display();
+
+    peek();
+
+    pop();
+
+    display();
+
+    return 0;
+}
